00375ff715a9f12d9e6eb5412d81b02bc4ecba4c
Jesus Cerron <jrcaguilar@gmail.com>
2025-12-31 23:04:26 +0200

    Conexion con strapi, proyectos

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,11 +2,12 @@
 
 import { useRouter } from "next/navigation";
 import BlogList from "@/components/BlogList";
+import type { BlogPost as BlogPostType } from "@/data/blog";
 import content from "@/data/content.json";
 
 export default function BlogPage() {
   const router = useRouter();
-  const blogPosts = content.blogPosts;
+  const blogPosts = content.blogPosts as BlogPostType[];
 
   return (
     <BlogList
