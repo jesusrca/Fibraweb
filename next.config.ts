7da92b913dfe195261881d7f8802e5e5b58160e6
Jesus Cerron <jrcaguilar@gmail.com>
2026-01-01 03:35:12 +0200

    Conexion con strapi, proyectos

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,16 @@ const nextConfig = {
         port: '1337', // El puerto donde vive Strapi
         pathname: '/uploads/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'mindful-butterfly-86d5d20e3b.strapiapp.com', // <--- TU NUEVO DOMINIO DE STRAPI
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // (Opcional: Por si Strapi Cloud usa Cloudinary interno)
+      },
     ],
   },
 };
 export default nextConfig;
+
